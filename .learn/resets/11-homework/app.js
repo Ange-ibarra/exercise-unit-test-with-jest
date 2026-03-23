2c779e3cc2c570978183ef78f96be75d812fd34f
Angelica Ibarra <angelicaavy@hotmail.es>
2026-03-23 19:29:58 +0000

    done

---
+++ .learn/resets/11-homework/app.js
@@ -0,0 +1,26 @@
+// Declaramos una función con el nombre exacto "formEuroToDollar"
+const fromEuroToDollar = function(valueInEuro) {
+    // Convertimos el valor a dólares
+    let valueInDollar = valueInEuro * 1.07;
+    // Retornamos el valor en dólares
+    return valueInDollar;
+}
+// Declaramos una función con el nombre exacto "formEuroToDollar"
+const fromDollarToYen= function(valueInDollar) {
+    // Convertimos el valor a dólares
+    let valueInYen = valueInDollar * 156.5;
+    // Retornamos el valor en dólares
+    return valueInYen;
+}
+// Declaramos una función con el nombre exacto "formEuroToDollar"
+const fromYenToPound = function(valueInEuro) {
+    // Convertimos el valor a dólares
+    let valueInDollar = valueInEuro * 1.07;
+    // Retornamos el valor en dólares
+    return valueInDollar;
+}
+
+
+module.exports = { sum, fromEuroToDollar }
+module.exports = { sum, fromDollarToYen }
+module.exports = { sum, fromYenToPound }
