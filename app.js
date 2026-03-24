8c82296eefbacde6941bceecd2c47d3330ef8e5d
Angelica Ibarra <angelicaavy@hotmail.es>
2026-03-24 20:35:25 +0000

    jest corregido

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 let oneEuroIs = {
     "JPY": 156.5, // japan yen
-    "USD": 1.07, // us dollar
-    "GBP": 0.87, // british pound
+    "USD": 1.07,  // us dollar
+    "GBP": 0.87   // british pound
 }
+
 const fromEuroToDollar = function(valueInEuro) {
-    let valueInDollar = valueInEuro * 1.07;
+    let valueInDollar = valueInEuro * oneEuroIs.USD;
     return valueInDollar;
 }
-const fromDollarToYen= function(valueInDollar) {
-    let valueInYen = valueInDollar * 156.5;
-    return valueInYen;
+
+const fromDollarToYen = function(valueInDollar) {
+    let euros = valueInDollar / oneEuroIs.USD;
+    let yen = euros * oneEuroIs.JPY;
+    return yen;
 }
 
 const fromYenToPound = function(valueInYen) {
-    let valueInPound = valueInYen * 0.87;
-    return valueInPound;
+    let euros = valueInYen / oneEuroIs.JPY;
+    let pounds = euros * oneEuroIs.GBP;
+    return pounds;
 }
 
-module.exports = {fromEuroToDollar, fromDollarToYen, fromYenToPound}
+module.exports = { fromEuroToDollar, fromDollarToYen, fromYenToPound };
+
--- test.js
+++ test.js
@@ -3,12 +3,12 @@ test("One euro should be 1.07 dollars", function() {
     expect(fromEuroToDollar(3.5)).toBe(3.745);
 })
 
-test("Conversion from USD to JPY (1 USD = 149.03 JPY)", function() {
+test("Conversion from USD to JPY using original rates", function() {
     const { fromDollarToYen } = require('./app.js')
-    expect(fromDollarToYen(10)).toBe(1490.3);
+    expect(fromDollarToYen(10)).toBe(1462.6168224299065);
 })
 
-test("Conversion from JPY to GBP (1 JPY = 0.0072 GBP)", function() {
+test("Conversion from JPY to GBP using original rates", function() {
     const { fromYenToPound } = require('./app.js')
-    expect(fromYenToPound(1000)).toBe(7.2);
+    expect(fromYenToPound(1000)).toBe(5.559105431309905);
 })
